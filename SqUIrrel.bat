47d02ac20c3eb7b391ac7a96f31e7ac4fa1112db
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-09 12:54:10 +0100

    Update SqUIrrel.bat

--- SqUIrrel.bat
+++ SqUIrrel.bat
@@ -25,39 +25,21 @@ if %udefine%==3 goto num
 if %udefine%==4 goto fun
 if %udefine%==5 goto set
 
-:ed
-cls
+:clo
 echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍSqUIrrel ManagerÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»
 echo º                                                º
 echo º        Welcome Please select an option         º
 echo º                                                º
 echo º                                                º
-echo º   [Edit]          ]  1. SqUIrrel Pad           º
-echo º    Terminal       ]  x. Exit                   º
-echo º    Numbers        ]                            º
+echo º    Edit           ]  1. Clock                  º
+echo º    Terminal       ]  2. Calculator             º
+echo º   [Numbers]       ]  x. Exit                   º
 echo º    Fun            ]                            º
-echo º    Settings       ]                            º
-echo º                   ]                            º
-echo ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
-echo.
+echo º    Settings       ]                    ÉÍÍÍÍÍÍÍÍClockÍÍÍÍÍxÍ»
+echo º                   ]                    º                    º
+echo ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍº  Time is: %time%   º
+echo                                          º                    º
+echo                                          ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
 set /p udefine= :
-if %udefine%==1 goto pad
 if %udefine%==x goto menu
 
-:ed
-cls
-echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍSqUIrrel ManagerÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ    TERMINAL    ÍÍÍÍÍÍÍÍÍÍÍÍÍÍxÍ»
-echo º                                                ºº  Welcome to SqUIrrel Terminal                  º
-echo º        Welcome Please select an option         ºº                                                º
-echo º                                                ºº   input: %input%                               º
-echo º                                                ºº   response: %resp%                             º
-echo º 1. Edit                                        ºº                                                º
-echo º 2. [Terminal]                                  ºº                                                º
-echo º 3. Numbers                                     ºº                                                º
-echo º 4. Fun                                         ºº                                                º
-echo º 5. Settings                                    ºº                                                º
-echo º                                                ºº                                                º
-echo ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
-
-set /p input= :
-if %input%==x goto menu
