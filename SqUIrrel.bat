d57c5eb6bf995aa114e541a699184dabce90abf7
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-09 11:31:06 +0100

    Update SqUIrrel.bat

--- SqUIrrel.bat
+++ SqUIrrel.bat
@@ -4,12 +4,12 @@ color F1
 cls
 
 :menu
-echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍSqUIrrel ManagerÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»
-echo º                                           º
-echo º 1.                                        º
-echo º 2.                                        º
-echo º 3.                                        º
-echo º 4.                                        º
-echo º 5.                                        º
-echo º                                           º
-echo ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+echo ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍSqUIrrel ManagerÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»
+echo º                                                º
+echo º 1. Edit                                        º
+echo º 2. Terminal                                    º
+echo º 3. Numbers                                     º
+echo º 4. Fun                                         º
+echo º 5. Settings                                    º
+echo º                                                º
+echo ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
